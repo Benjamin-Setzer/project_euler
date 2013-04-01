2dd29134c8e77d201d049786b28791330c82b720
Benjamin Setzer <benjamin.setzer@gmx.de>
2013-04-02 00:05:00 +0200

    initial commit of 12

--- e12.py
+++ e12.py
@@ -1,23 +1,31 @@
 """
 http://projecteuler.net/problem=12
 """
+import math
 
 def find_tri_num(num_divisors):
+	lower_bound = math.factorial(10) 
+	#since we are looking for 500 unique divisors this is the lower bound
+	#
+	
 	increment = 1
 	triangle_number = 1
 
 	while True:
-		if len(get_divisors(triangle_number)) > num_divisors:
-			return triangleNumber
+		print lower_bound - triangle_number
+		if triangle_number >= lower_bound:
+			count_divisors = len(get_divisors(triangle_number))
+			print triangle_number, count_divisors
+			if count_divisors > num_divisors:
+				return triangle_Number
 		increment += 1
 		triangle_number += increment
-		print triangle_number
 		
 def get_divisors(num):
-	divisors = set()
-	for i in range(1, num+1):
+	divisors = []
+	for i in xrange(1, num+1):
 		if num % i == 0:
-			divisors.add(num)
+			divisors.append(i)
 	
 	return divisors
 
